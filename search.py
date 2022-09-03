68fdbf6bc2cddbbce6e2a90b8e28db333601358f
John Smith <johnlsmith1880@gmail.com>
2022-09-03 11:55:28 +0100

    start simple flask app and add filtering functionality

--- search.py
+++ search.py
@@ -1,30 +1,63 @@
-import pandas as pd
-import requests
+#! /usr/bin/python3
 
+import requests
+import pandas as pd
+
+from datetime import datetime
 from requests.exceptions import RequestException
 from storage import DBStorage
 from urllib.parse import quote_plus
 
-#from settings import *
+from settings import *
 
 def search_api(query, pages=int(RESULT_COUNT/10)):
-  '''queries custom search API end point and returns search results'''
-  result = []
-  
-  for i in range(0, pages):
-    start = i * 10 + i  #rank of first record on page returned (1, 11,...)
-    url = SEARCH_URL.format(
-        key=SEARCH_KEY,
-        cx=SEARCH_ID,
-        query=quote_plus(query),  #ensures query is properly (URL) formatted
-        start=start
-    )
-    
-    reponse = requests.get(url)
-    data = response.json()
-    results += data["items"]  #returns list of dicts
-  
-  res_df = pd.DataFrame.from_dict(results)
-  res_df["rank"] = list(range(1, res_df.shape[0] + 1))   #indicates rank (1-11)
-  res_df = res_df[["link", "rank", "snippet", "title"]]   #removed a few fields
-  return res_df
+    results = []
+    for i in range(0, pages):
+        start = i*10+1
+        url = SEARCH_URL.format(
+            key=SEARCH_KEY,
+            cx=SEARCH_ID,
+            query=quote_plus(query),
+            start=start
+        )
+        response = requests.get(url)
+        data = response.json()
+        results += data["items"]
+    res_df = pd.DataFrame.from_dict(results)
+    res_df["rank"] = list(range(1, res_df.shape[0] + 1))
+    res_df = res_df[["link", "rank", "snippet", "title"]]
+    return res_df
+
+
+def scrape_page(links):
+    html = []
+    for link in links:
+        print(link)
+        try:
+            data = requests.get(link, timeout=5)
+            html.append(data.text)
+        except RequestException:
+            html.append("")
+    return html
+
+
+def search(query):
+    columns = ["query", "rank", "link", "title", "snippet", "html", "created"]
+    storage = DBStorage()
+
+    stored_results = storage.query_results(query)
+    if stored_results.shape[0] > 0:
+        stored_results["created"] = pd.to_datetime(stored_results["created"])
+        return stored_results[columns]
+
+    print("No results in database.  Using the API.")
+    results = search_api(query)
+    html = scrape_page(results["link"])
+    results["html"] = html
+    results = results[results["html"].str.len() > 0].copy()
+    results["query"] = query
+    results["created"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    results = results[columns]
+    results.apply(lambda x: storage.insert_row(x), axis=1)
+    print(f"Inserted {results.shape[0]} records.")
+    return results
